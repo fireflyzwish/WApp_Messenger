a47b3bfdd96a77e36a447d29ff596225c80e48df
Ievgenii  Svitlychnyi <fireflyzwish@gmail.com>
2023-01-24 14:30:57 +0100

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,14 +1,16 @@
-import csv,re,time, keyboard, webbrowser, datetime #modules to import
-with open("*NAME*.csv") as csvfile: #*.CSV file should be named as *NAME*
-    numreader=csv.reader(csvfile, delimiter=';') #delimiter CSV
-    str_list = [] #make the list
-    for row in numreader: #open the file
-        phonenumber=re.sub(r'\D', '',row[11]).split() #elaborate phone numbers from row 11, replace all non-numeric symbols with '', using regular expressions 
-        message=("Dear "+row[9]+", you have an appointment at "+row[12]+" "+row[13]+". This message is a reminder, don't hesitate to contact us via mail or something else blah blah blah")
+import csv,re,time, keyboard, webbrowser, glob, datetime
+tomorrow = datetime.datetime.now() + datetime.timedelta(1) #tommorow DAY-MNTH-YEAR
+csvopen=glob.glob("*.csv") #any CSV file in a current folder
+with open(csvopen[0]) as csvfile:
+    numreader=csv.reader(csvfile, delimiter=';')
+    str_list = []
+    for row in numreader:
+        phonenumber=re.sub(r'\D', '',row[3]).split() #elaborate phone numbers from row 11, replace all non-numeric symbols with '', using regular expressions 
+        message=("TEXT1"+row[1]+"SPACE"+row[2]+"TEXT2"+tomorrow.strftime('%d-%m-%Y')+"TEXT3"+row[0]+".")
         for i in phonenumber:
-            webbrowser.open("https://web.whatsapp.com/send?phone=39"+i+"&text="+message) #open WhatsAPP API, and send messages
-            time.sleep(7)
+            webbrowser.open("https://web.whatsapp.com/send?phone=39"+i+"&text="+message) #open WhatsAPP WEB, and send messages
+            time.sleep(9)
             keyboard.press_and_release("enter")
-            time.sleep(5)
+            time.sleep(7)
             keyboard.press_and_release("ctrl+w")
             keyboard.press_and_release("enter")
